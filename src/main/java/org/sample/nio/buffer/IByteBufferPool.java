8a7d274c03a54e7382a0aab84a9e628f28a968ec
leon.chen <leon.chen@thomsonreuters.com>
2015-12-29 17:00:01 +0800

    first commit

--- src/main/java/org/sample/nio/buffer/IByteBufferPool.java
+++ src/main/java/org/sample/nio/buffer/IByteBufferPool.java
@@ -2,13 +2,7 @@ package org.sample.nio.buffer;
 
 import java.nio.ByteBuffer;
 
-<<<<<<< HEAD
-/**
- * Created by U0128754 on 12/23/2015.
- */
-=======
 
->>>>>>> first commit
 public interface IByteBufferPool {
 
     public ByteBuffer acquire();
